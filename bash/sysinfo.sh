2d1819637d50701a1bbd49f6d0ddfbe37702c65a
harsh harsh <hhrsh05@gmail.com>
2022-10-07 13:23:36 -0400

    improved sysinfo

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#My lab 1 script 
-#echo will show us the whatever you wirte in front of it  
-echo "FQDN:"
-#hostname -A will tell us the hostname 
-hostname -A
-echo "Host Inforamtion:"
-#hostnamectl will give us the all information about the operating system 
-hostnamectl   
-echo "IP Addresses :" 
-#these both commands will give us the IP addresses 
-ip a s ens33 | grep -w inet | awk '{print $2}'
-ip a s ens33 | grep -w inet6 | awk '{print $2}'
-echo "Root Filesystem status:"
-#df -h will give us the information about rootfile system  
-df -h /dev/sda3
+#My lab 2 script improved sysinfo.sh
+#gathering all the data
+fqdn=$(hostname -f)
+osname=$(cat /etc/os-release | grep -w NAME)
+osversion=$(cat /etc/os-release | grep -w VERSION)
+ipaddress=$(ip a s ens33 | grep -w inet | awk '{print $2}' | sed -e 's,/..*,,')
+availablespace=$(df -h --output=avail / |grep Avail -v) 
+#for outputemplate
+cat <<EOF
+"Report for $HOSTNAME"
+======================
+FQDN: $fqdn
+Operating System name and version : $osname and $osversion
+IP Address : $ipaddress
+Root Filesystem Free Space: $availablespace
+======================
+EOF
 
