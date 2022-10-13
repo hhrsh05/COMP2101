f769c8b2a72e4cde595d31ff1c177246ff5722d3
harsh harsh <hhrsh05@gmail.com>
2022-10-13 08:42:17 -0400

    updated

--- bash/vwebserver.sh
+++ bash/vwebserver.sh
@@ -20,11 +20,17 @@ if [ $? -ne 0 ]; then
 		exit 1
         fi
 fi
+#this will create our container  
 lxc launch ubuntu:20.04  COMP2101-S22
+#lxc list will show us containers 
 lxc list 
 containerIP=$(lxc list | grep -w IPV4 -v | awk '{ print$6 }')
+sudo -- sh -c "echo $containerIP COMP2101-S22 >> /etc/hosts"
+#this will install apache2 in conatiner 
 lxc exec COMP2101-S22 -- apt install apache2
+#this will start the apache2 web server 
 lxc exec COMP2101-S22 -- systemctl start apache2
+#curl will test that it is working 
 curl http://comp2101-s22/
 if [ $? -eq 0 ]; then
 	echo "successfull"
