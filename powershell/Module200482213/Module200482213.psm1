7f6791a800894a562be54c2d49d318415fd36810
HARSH Harsh <hhrsh05@gmail.com>
2022-12-15 08:07:46 -0500

    Create Module200482213.psm1
    module

---
+++ powershell/Module200482213/Module200482213.psm1
@@ -0,0 +1,16 @@
+﻿function welcome {
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now."
+}
+welcome
+function get-cpuinfo {
+ get-ciminstance cim_processor | format-list Manufacturer, Name, CurrentClockSpeed, MaxClockSpeed, NumberOfCores
+}
+get-cpuinfo
+function get-mydisks {
+get-physicaldisk | ft Manufacturer, Model, SerialNumber, Firmwareversion, Size  -autosize
+}
+get-mydisks
+
+
